1551039ad6cbf820d4e8a423180cddb24bb9e3f0
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-06-28 23:05:03 +0600

    UI Design - 8

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,6 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import TopBanner from "../component/topContent/TopBanner";
-import CourseList from "../component/Course/CourseList";
+import CourseList from "../component/course/CourseList";
 import TopNavigation from '../component/topContent/TopNavigation';
 
 class HomePage extends Component {

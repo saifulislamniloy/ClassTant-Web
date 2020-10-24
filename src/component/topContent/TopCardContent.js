27ed4d4fa73ecf62636959e582c8fbb0163ce1a9
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-24 10:15:08 +0600

    get course, get and post course link added

--- src/component/topContent/TopCardContent.js
+++ src/component/topContent/TopCardContent.js
@@ -7,65 +7,84 @@ import firebase from "firebase";
 export default class TopCardContent extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            selectedCourse:"Select Course",
-            isCourseSelected:false,
-            selectedCourseName:"Course Name",
-            selectedCourseCode:"Course Code",
-            selectedCourseId:"",
-            courseListView:"",
+        this.state = {
+            selectedCourse: "Select Course",
+            isCourseSelected: false,
+            selectedCourseName: "Course Name",
+            selectedCourseCode: "Course Code",
+            selectedCourseId: "",
+            courseListView: "",
 
-            classLink:""
+            classLink: "",
+            editMode: false
         }
     }
+
     componentDidMount() {
         this.getCourseList()
     }
 
-    getCourseList(){
+    getCourseList() {
         const db = firebase.database();
-        db.ref("Teachers/"+this.props.uid+"/courseList").once("value" )
-            .then(snapshot =>{
+        db.ref("Teachers/" + this.props.uid + "/courseList").once("value")
+            .then(snapshot => {
 
                 const courses = snapshot.val();
                 console.log(courses)
                 const courseId = []
-                for (var key in courses){
+                for (var key in courses) {
                     courseId.push(key)
-                    console.log(key +" "+ courses[key]["courseName"])
+                    console.log(key + " " + courses[key]["courseName"])
                 }
 
                 const view = courseId.map(courseId => {
                     return (
                         <Dropdown.Item
-                            onClick={()=>{
-                                this.getClassLink()
+                            onClick={() => {
                                 this.setState(
-                                    {isCourseSelected:true,
+                                    {
+                                        isCourseSelected: true,
                                         selectedCourse: courses[courseId]["courseName"],
                                         selectedCourseName: courses[courseId]["courseName"],
-                                        selectedCourseCode:courses[courseId]["courseCode"],
-                                        selectedCourseId:courses[courseId]["courseID"]})}}>
+                                        selectedCourseCode: courses[courseId]["courseCode"],
+                                        selectedCourseId: courses[courseId]["courseID"]
+                                    })
+
+                                this.getClassLink(courses[courseId]["courseID"])
+                            }}>
                             {courses[courseId]["courseName"]}
                         </Dropdown.Item>
                     )
                 })
-                this.setState({courseListView:view})
-        })
-    }
-
-    getClassLink(){
-        const db = firebase.database();
-        db.ref("Test/classLink").once("value" )
-            .then(snapshot =>{
-                this.setState({classLink:snapshot.val()})
+                this.setState({courseListView: view})
             })
     }
 
-    postClassLink(){
+    getClassLink(courseId) {
         const db = firebase.database();
-        db.ref("test/classLink").update({"link":document.getElementById("classLink").value})
+        db.ref("Courses/" + courseId + "/courseInfo").once("value")
+            .then(snapshot => {
+                let link = snapshot.val().classLink
+                console.log(snapshot.val())
+                this.setState({classLink: link})
+            })
     }
+
+    postClassLink() {
+        const db = firebase.database();
+        if (this.state.selectedCourseId !== "")
+            db.ref("Courses/" + this.state.selectedCourseId + "/courseInfo")
+                .update({"classLink": document.getElementById("classLink").value},
+                    function (error) {
+                        if (error)
+                            alert("failed")
+                        else
+                            alert("success")
+                    })
+        else
+            alert("Select a course!")
+    }
+
 
     render() {
         return (
@@ -74,11 +93,12 @@ export default class TopCardContent extends Component {
                     <Row>
                         <Col sm={12} md={12} lg={12}>
                             <div style={{margin: 20}}>
-                                <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
+                                <Card style={{padding: 10, textAlign: "center", boxShadow: 10}}>
                                     <Row>
                                         <Col sm={12} md={6} lg={6}>
                                             <Dropdown>
-                                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                                                <Dropdown.Toggle variant="primary" id="dropdown-basic"
+                                                                 style={{background: "#4396F8"}}>
                                                     {this.state.selectedCourse}
                                                 </Dropdown.Toggle>
                                                 <Dropdown.Menu>
@@ -86,49 +106,69 @@ export default class TopCardContent extends Component {
                                                 </Dropdown.Menu>
                                             </Dropdown>
 
-                                            <Button style={{margin: 10}}><Link to="create-course"
-                                                                               style={{color: "white"}}>Create
-                                                Course</Link></Button>
+                                            <Card.Text>{this.state.selectedCourseName}</Card.Text>
                                         </Col>
                                         <Col sm={12} md={6} lg={6}>
-                                            <Card.Text>{this.state.selectedCourseName}</Card.Text>
+                                            <Button style={{background: "#4396F8"}}><Link to="create-course"
+                                                                                          style={{color: "white"}}>Create
+                                                Course</Link></Button>
+
                                             <Card.Text>{this.state.selectedCourseCode}</Card.Text>
                                         </Col>
                                     </Row>
-                                </Card>
-                            </div>
-                        </Col>
-                        <Col sm={12} md={12} lg={12}>
-                            <div style={{margin: 20}}>
-                                <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
-                                    <Row>
-                                        <Col sm={2} md={3} lg={4}>
-                                            <Card.Title>Class Link</Card.Title>
-                                        </Col>
-                                        <Col sm={8} md={7} lg={6}>
-                                            <Form>
-                                                <Form.Group>
-                                                    <Form.Control id="classLink" type="text" placeholder="Enter Class link" />
-                                                </Form.Group>
-                                            </Form>
-                                        </Col>
-                                        <Col sm={2} md={2} lg={2}>
-                                            <Button onClick={() => this.postClassLink()} variant="primary">
-                                                Save
-                                            </Button>
-                                        </Col>
+                                    <br/>
+                                    <Row className="justify-content-center">
+
+                                        {
+                                            this.state.editMode ?
+                                                <Row >
+                                                    <Col sm={2} md={3} lg={4}>
+                                                        <Card.Title>Class Link</Card.Title>
+                                                    </Col>
+                                                    <Col sm={8} md={7} lg={6}>
+                                                        <Form>
+                                                            <Form.Group>
+                                                                <Form.Control id="classLink" type="text"
+                                                                              placeholder="Enter Class link"/>
+                                                            </Form.Group>
+                                                        </Form>
+                                                    </Col>
+                                                    <Col sm={2} md={2} lg={2}>
+                                                        <Button onClick={() => {
+                                                            this.postClassLink();
+                                                            this.setState({editMode:false})
+                                                        }} variant="primary"  style={{background: "#4396F8"}}>
+                                                            Save
+                                                        </Button>
+                                                    </Col>
+                                                </Row>
+
+                                                :
+                                                this.state.isCourseSelected ?
+                                                    <Row className="">
+                                                        <Col>
+                                                            <p>{this.state.classLink}</p>
+                                                        </Col>
+                                                        <Col>
+                                                            <Button onClick={()=> this.setState({editMode:true})}>Edit</Button>
+                                                        </Col>
+                                                    </Row>
+                                                    :
+                                                    <h1></h1>
+                                        }
                                     </Row>
                                 </Card>
                             </div>
                         </Col>
 
+
                     </Row>
-                    <Row>
+                    {this.state.isCourseSelected ? <Row>
                         {/* Annoucnement */}
                         <Col sm={6} md={6} lg={4}>
                             <div style={{margin: 20}}>
-                                <Link to={"/announcement/"+this.state.selectedCourseId}>
-                                    <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
+                                <Link to={"/announcement/" + this.state.selectedCourseId}>
+                                    <Card style={{padding: 10, textAlign: "center"}}>
                                         <Card.Title>
                                             Announcement
                                         </Card.Title>
@@ -142,7 +182,7 @@ export default class TopCardContent extends Component {
                         {/* DIscussion */}
                         <Col sm={6} md={6} lg={4}>
                             <div style={{margin: 20}}>
-                                <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
+                                <Card style={{padding: 10, textAlign: "center"}}>
                                     <Card.Title>
                                         DIscussion
                                     </Card.Title>
@@ -155,7 +195,7 @@ export default class TopCardContent extends Component {
                         {/* Class Schedule */}
                         <Col sm={6} md={6} lg={4}>
                             <div style={{margin: 20}}>
-                                <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
+                                <Card style={{padding: 10, textAlign: "center"}}>
                                     <Card.Title>
                                         Class Schedule
                                     </Card.Title>
@@ -168,22 +208,22 @@ export default class TopCardContent extends Component {
                         {/* Assignments */}
                         <Col sm={6} md={6} lg={4}>
                             <div style={{margin: 20}}>
-                               <Link to="/assignment">
-                                   <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
-                                       <Card.Title>
-                                           Assignments
-                                       </Card.Title>
-                                       <Card.Body>
-                                           A declaration you want to share among the class
-                                       </Card.Body>
-                                   </Card>
-                               </Link>
+                                <Link to="/assignment">
+                                    <Card style={{padding: 10, textAlign: "center"}}>
+                                        <Card.Title>
+                                            Assignments
+                                        </Card.Title>
+                                        <Card.Body>
+                                            A declaration you want to share among the class
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
                             </div>
                         </Col>
                         {/* Marks */}
                         <Col sm={6} md={6} lg={4}>
                             <div style={{margin: 20}}>
-                                <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
+                                <Card style={{padding: 10, textAlign: "center"}}>
                                     <Card.Title>
                                         Marks
                                     </Card.Title>
@@ -196,7 +236,7 @@ export default class TopCardContent extends Component {
                         {/* Appointments */}
                         <Col sm={6} md={6} lg={4}>
                             <div style={{margin: 20}}>
-                                <Card style={{padding: 10, textAlign: "center", color: "white", background: "blue"}}>
+                                <Card style={{padding: 10, textAlign: "center"}}>
                                     <Card.Title>
                                         Appointments
                                     </Card.Title>
@@ -206,7 +246,8 @@ export default class TopCardContent extends Component {
                                 </Card>
                             </div>
                         </Col>
-                    </Row>
+                    </Row> : <h1></h1>}
+
                 </Container>
             </Fragment>
         )

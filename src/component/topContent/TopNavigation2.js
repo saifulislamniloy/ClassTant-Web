f1ab64a17072249b80413f7f00a22ec03f986f67
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-24 10:25:18 +0600

    icon added

--- src/component/topContent/TopNavigation2.js
+++ src/component/topContent/TopNavigation2.js
@@ -4,6 +4,7 @@ import '../../asset/css/bootstrap.min.css';
 import {Container, Navbar, Nav, Button} from 'react-bootstrap'
 import {NavLink} from "react-router-dom";
 import {auth} from "../../firebase";
+import icon from "../../asset/image/home-icon.png";
 
 export default class TopNavigation2 extends Component {
     logout(){
@@ -14,7 +15,7 @@ export default class TopNavigation2 extends Component {
             <Fragment>
                 <Container>
                     <Navbar fixed="top" bg="light" variant="light" collapseOnSelect expand="lg">
-                        <Navbar.Brand className="title" href="/" ><img src="https://s3.amazonaws.com/somewherein/assets/images/thumbs/iutbd_1363355287_2-IUT_1.209173425.jpg" height="32" width="32" alt="Smiley face"/>  Classtant</Navbar.Brand>
+                        <Navbar.Brand className="title" href="/" ><img src={icon} height="32" width="32" alt="Smiley face"/>  Classtant</Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav" >
                             <Nav className="mr-auto">

14c3dfe18b2f577a553a56e11b062aa46d50fca3
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-27 11:08:09 +0600

    new directory added for course

--- src/page/CreateCoursePage.js
+++ src/page/CreateCoursePage.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import CreateCourse from "../component/outsideCourse/CreateCourse.js";
+import CreateCourse from "../component/course/CreateCourse.js";
 import TopNavigation2 from '../component/topContent/TopNavigation2.js';
 
 class CreateCoursePage extends Component {

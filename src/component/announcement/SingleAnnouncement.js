99480d6fee312d316d1e38c71cc940295a3e8802
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-29 17:16:12 +0600

    announcement delete done

--- src/component/announcement/SingleAnnouncement.js
+++ src/component/announcement/SingleAnnouncement.js
@@ -9,6 +9,7 @@ class SingleAnnouncement extends Component {
         super();
         this.state={
             editMode:false,
+            isDeleted:false,
             title:"",
             description:"",
             link:""
@@ -62,102 +63,124 @@ class SingleAnnouncement extends Component {
             return true
     }
 
+    deleteSingleAnnouncement(){
+        const db = firebase.database();
+        const time = new Date().getTime().toString();
+        if (this.validation()) {
+            db.ref("Courses/" + this.props.courseId + "/announcements/" + this.props.id)
+                .remove(onerror=>{
+                    if(onerror){
+                        alert("Failed!")
+                    }else {
+                        this.setState({isDeleted:true})
+                    }
+                })
+        }
+    }
+
 
     render() {
         return (
             <Fragment>
-                <Card className="topCardDesign">
-                    <Card.Header>
-                        <Row>
-                            <Col sm={6} md={10} lg={10} className="cardTitle">
-                                {
-                                    this.state.editMode ?
-                                        <Form.Group>
-                                            <Form.Control id="header" type="text" placeholder="Title"
-                                                          value={this.state.title}
-                                                            onChange={(text)=>{this.setState({title:text.target.value})}}/>
-                                        </Form.Group>
-                                        :
-                                        this.state.title
-                                }
-                            </Col>
-                            <Col sm={6} md={2} lg={2}>
-                                {
-                                    this.state.editMode ?
-                                        <h1></h1>
-                                        :
-                                        <Dropdown>
-                                            <Dropdown.Toggle variant="primary">
-                                            </Dropdown.Toggle>
-                                            <Dropdown.Menu>
-                                                <Dropdown.Item onClick={()=>this.setState({editMode:true})}>
-                                                    Edit
-                                                </Dropdown.Item>
-                                                <Dropdown.Item className="disabled">
-                                                    Delete (Coming Soon)
-                                                </Dropdown.Item>
-                                            </Dropdown.Menu>
-                                        </Dropdown>
-                                }
-                            </Col>
-                        </Row>
-                    </Card.Header>
-                    {
-                        this.state.editMode
-                            ?
-                            <Card.Body>
-                               <Form>
-                                   <Form.Group>
-                                       <Form.Control id="des" as="textarea" rows={3} placeholder="Description"
-                                                     value={this.state.description}
-                                                     onChange={(des)=>{this.setState({description:des.target.value})}}/>
-                                   </Form.Group>
-                                   <Form.Group>
-                                       <Form.Control id="link" type="text" placeholder="Link (Optional)"
-                                                     value={this.state.link}
-                                                     onChange={(link)=>{this.setState({link:link.target.value})}}/>
-                                   </Form.Group>
-                               </Form>
-                            </Card.Body>
-                            :
-                            <Card.Body>
-                                <p className="cardBody alignLeft">{this.state.description}</p>
+                {
+                    this.state.isDeleted ?
+                        <Card className="deletedCard">
+                            <Card.Title>Deleted</Card.Title>
+                        </Card>
+                        :
+                        <Card className="topCardDesign">
+                            <Card.Header>
+                                <Row>
+                                    <Col sm={6} md={10} lg={10} className="cardTitle">
+                                        {
+                                            this.state.editMode ?
+                                                <Form.Group>
+                                                    <Form.Control id="header" type="text" placeholder="Title"
+                                                                  value={this.state.title}
+                                                                  onChange={(text)=>{this.setState({title:text.target.value})}}/>
+                                                </Form.Group>
+                                                :
+                                                this.state.title
+                                        }
+                                    </Col>
+                                    <Col sm={6} md={2} lg={2}>
+                                        {
+                                            this.state.editMode ?
+                                                <h1></h1>
+                                                :
+                                                <Dropdown>
+                                                    <Dropdown.Toggle variant="primary">
+                                                    </Dropdown.Toggle>
+                                                    <Dropdown.Menu>
+                                                        <Dropdown.Item onClick={()=>this.setState({editMode:true})}>
+                                                            Edit
+                                                        </Dropdown.Item>
+                                                        <Dropdown.Item onClick={()=>this.deleteSingleAnnouncement()}>
+                                                            Delete
+                                                        </Dropdown.Item>
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+                                        }
+                                    </Col>
+                                </Row>
+                            </Card.Header>
+                            {
+                                this.state.editMode
+                                    ?
+                                    <Card.Body>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Control id="des" as="textarea" rows={3} placeholder="Description"
+                                                              value={this.state.description}
+                                                              onChange={(des)=>{this.setState({description:des.target.value})}}/>
+                                            </Form.Group>
+                                            <Form.Group>
+                                                <Form.Control id="link" type="text" placeholder="Link (Optional)"
+                                                              value={this.state.link}
+                                                              onChange={(link)=>{this.setState({link:link.target.value})}}/>
+                                            </Form.Group>
+                                        </Form>
+                                    </Card.Body>
+                                    :
+                                    <Card.Body>
+                                        <p className="cardBody alignLeft">{this.state.description}</p>
 
-                                <a href={"https://" + this.state.link}
-                                   target="#">{this.state.link}</a>
-                            </Card.Body>
-                    }
-                    <Card.Footer>
-                        {
-                            this.state.editMode
-                            ?
-                                <Row>
-                                    <Col>
-                                        <Button
-                                            onClick={()=>{
-                                                this.setState({editMode:false})
-                                                this.getSingleAnnouncement()}}>Cancel</Button>
-                                    </Col>
-                                    <Col>
-                                        <Button
-                                            onClick={()=>{
-                                                this.setState({editMode:false})
-                                                this.updateSingleAnnouncement()
-                                                this.getSingleAnnouncement()}}>Update</Button>
-                                    </Col>
-                                </Row>
-                                :
-                                <Row>
-                                    <Col sm={6} md={6} lg={6}>
-                                        <p className="cardFooter">~Author: {this.props.postedBy}</p>
-                                    </Col>
-                                    <Col sm={6} md={6} lg={6}>
-                                        <p className="cardFooter">{"~Posted on: "+getDateTime(this.props.postTime)}</p>
-                                    </Col>
-                                </Row>
-                        }
-                    </Card.Footer>
-                </Card>
+                                        <a href={"https://" + this.state.link}
+                                           target="#">{this.state.link}</a>
+                                    </Card.Body>
+                            }
+                            <Card.Footer>
+                                {
+                                    this.state.editMode
+                                        ?
+                                        <Row>
+                                            <Col>
+                                                <Button
+                                                    onClick={()=>{
+                                                        this.setState({editMode:false})
+                                                        this.getSingleAnnouncement()}}>Cancel</Button>
+                                            </Col>
+                                            <Col>
+                                                <Button
+                                                    onClick={()=>{
+                                                        this.setState({editMode:false})
+                                                        this.updateSingleAnnouncement()
+                                                        this.getSingleAnnouncement()}}>Update</Button>
+                                            </Col>
+                                        </Row>
+                                        :
+                                        <Row>
+                                            <Col sm={6} md={6} lg={6}>
+                                                <p className="cardFooter">~Author: {this.props.postedBy}</p>
+                                            </Col>
+                                            <Col sm={6} md={6} lg={6}>
+                                                <p className="cardFooter">{"~Posted on: "+getDateTime(this.props.postTime)}</p>
+                                            </Col>
+                                        </Row>
+                                }
+                            </Card.Footer>
+                        </Card>
+                }
             </Fragment>
         );
     }

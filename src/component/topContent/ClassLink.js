b4ac0aeb83bba35bc465f2be98b0eea9c69d2354
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-12-28 18:48:10 +0600

    ClassLink Component Modified

--- src/component/topContent/ClassLink.js
+++ src/component/topContent/ClassLink.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import firebase from "firebase";
 import { CourseContext } from '../../providers/CourseProvider'
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { Row, Col, Button, Card } from 'react-bootstrap';
 import EmptySpace from '../common/EmptySpace';
 
 export default class ClassLink extends Component {
@@ -9,13 +8,13 @@ export default class ClassLink extends Component {
         return (
             <CourseContext.Consumer>
                 {(course) => (
-                    <Container>
-                        {
-                            course.isCourseSelected + course.classLinkEditMode === 1 ?
+                    <span>
+                        {course.isCourseSelected + course.classLinkEditMode === 1 ?
+                            <Card.Body>
                                 <Row>
                                     <Col sm={10} md={10} lg={10}>
                                         <Card.Title className="classLink mt-2">
-                                            Class Link: {course.classLink === undefined ? "No link given. Click Edit to  provide link." : <a href={course.classLink} target="#">{course.classLink}</a>}
+                                            Class Link: {course.classLink === undefined ? "No link given. Click edit to  provide link." : <a href={course.classLink} target="#">{course.classLink}</a>}
                                         </Card.Title>
                                     </Col>
                                     <Col sm={2} md={2} lg={2}>
@@ -24,10 +23,11 @@ export default class ClassLink extends Component {
                                         </Button>
                                     </Col>
                                 </Row>
-                                :
-                                <EmptySpace />
+                            </Card.Body>
+                            :
+                            <EmptySpace />
                         }
-                    </Container>
+                    </span>
                 )}
             </CourseContext.Consumer>
         )
